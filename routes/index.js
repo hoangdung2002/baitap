b083b2cc3a239cf80d4f7b654ec10cbe943dc2f9
hoangdung2002 <dunghhph14594@fpt.edu.vn>
2022-04-08 14:55:21 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    var random = Math.random();
+    cb(null, random +  Date.now() + file.originalname);
+  },
+});
+
+
+var upload = multer({ storage: storage, limits: {
+    fileSize: 2 * 1024 * 1024
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/profile', upload.array('avatar', 5), function (req, res){
+  res.render('index', {title: 'Upload thành công!!!' });
+});
 module.exports = router;
